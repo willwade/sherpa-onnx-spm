6bcdc10422455573ee50f405f380ef9b546f3a73
will wade <willwade@gmail.com>
2026-05-21 05:34:51 +0100

    Fix: manually construct merged xcframework with iOS + macOS slices

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import PackageDescription
 
 let version = "1.13.2"
-let checksumSherpaOnnx = "cdacade0f053821a74d557b3aa98bad33d79b3e7be6fc194215775e5501c66a8"
-let checksumOnnxRuntime = "e4df27a40ecb6c05304d26142ef32f107843a531ca308e1a860aa87d851c4817"
+let checksumSherpa = "placeholder"
+let checksumOrt = "placeholder"
 
 let package = Package(
     name: "SherpaOnnx",
@@ -22,12 +22,12 @@ let package = Package(
         .binaryTarget(
             name: "sherpa-onnx",
             url: "https://github.com/willwade/sherpa-onnx-spm/releases/download/\(version)/sherpa-onnx.xcframework.zip",
-            checksum: checksumSherpaOnnx
+            checksum: checksumSherpa
         ),
         .binaryTarget(
             name: "onnxruntime",
             url: "https://github.com/willwade/sherpa-onnx-spm/releases/download/\(version)/onnxruntime.xcframework.zip",
-            checksum: checksumOnnxRuntime
+            checksum: checksumOrt
         ),
         .target(
             name: "SherpaOnnx",
