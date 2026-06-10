78c979f0746d6ac449a69b94f9cec98a5e78c2ce
will wade <willwade@gmail.com>
2026-06-10 14:06:01 +0100

    fix: update v1.13.2 checksums to match release assets
    The v1.13.2 release assets were updated but the Package.swift checksums were not synchronized. This updates the checksums to match the actual assets in the release.

--- Package.swift
+++ Package.swift
@@ -2,8 +2,9 @@
 
 import PackageDescription
 
-let version = "1.13.6"
-let checksumCombined = "66fbec263ff19b26343a28a1e30be58ae642d2feb6c306fdaa3d6df9e5257bfd"
+let version = "1.13.2"
+let checksumSherpa = "0017147fda683ce99f9390b43825c529966ad29b40ac67e37a43f2ba61d5264d"
+let checksumOrt = "8c9db6bff51a7282a7b3ad08b9e87a88b41f03e02bf8e7dbbd53c6a66df75512"
 
 let package = Package(
     name: "SherpaOnnx",
@@ -21,11 +22,16 @@ let package = Package(
         .binaryTarget(
             name: "sherpa-onnx",
             url: "https://github.com/willwade/sherpa-onnx-spm/releases/download/\(version)/sherpa-onnx.xcframework.zip",
-            checksum: checksumCombined
+            checksum: checksumSherpa
+        ),
+        .binaryTarget(
+            name: "onnxruntime",
+            url: "https://github.com/willwade/sherpa-onnx-spm/releases/download/\(version)/onnxruntime.xcframework.zip",
+            checksum: checksumOrt
         ),
         .target(
             name: "SherpaOnnx",
-            dependencies: ["sherpa-onnx"],
+            dependencies: ["sherpa-onnx", "onnxruntime"],
             path: "Sources/SherpaOnnx",
             linkerSettings: [
                 .linkedLibrary("c++"),
