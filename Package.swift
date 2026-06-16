c9654f31f19ed43de8178aa758d193f625983ce8
will wade <willwade@gmail.com>
2026-06-16 22:03:00 +0100

    Fix checksums to match downloaded artifacts

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import PackageDescription
 
 let version = "1.13.2"
-let checksumSherpa = "62de3c1423a4f20516e8623858ee8c8d306af7ebb2a3737dc0600b1d4ee6aa4b"
-let checksumOrt = "38bc65b3e6af3e6d99bc18a40f80bfb3e56ee1eedfa0d0a60feb1c97a2d06dee"
+let checksumSherpa = "0017147fda683ce99f9390b43825c529966ad29b40ac67e37a43f2ba61d5264d"
+let checksumOrt = "8c9db6bff51a7282a7b3ad08b9e87a88b41f03e02bf8e7dbbd53c6a66df75512"
 
 let package = Package(
     name: "SherpaOnnx",
